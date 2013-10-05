3407f279f14f9b1d4f4051e0082a07b9a0709fcb
Shinsuke KUROKI <s-kuroki@aiming-inc.com>
2013-10-05 15:56:24 +0900

    Add JavaVersionParser.judge_validity

--- lib/java_version_parser.rb
+++ lib/java_version_parser.rb
@@ -1,2 +1,5 @@
 class JavaVersionParser
+  def self.judge_validity(source)
+    /JDK\d+u\d+/ === source
+  end
 end
--- spec/lib/java_version_parser_spec.rb
+++ spec/lib/java_version_parser_spec.rb
@@ -1,5 +1,9 @@
 require 'java_version_parser'
 
 describe JavaVersionParser do
-  pending
+  describe '.judge_validity' do
+    specify { JavaVersionParser.judge_validity("JDK7u40").should be_true }
+    specify { JavaVersionParser.judge_validity("JDK740").should be_false }
+    specify { JavaVersionParser.judge_validity("JDK7u").should be_false }
+  end
 end
