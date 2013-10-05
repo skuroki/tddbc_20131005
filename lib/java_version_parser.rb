8e5d6ce91ff46c4494f76101ad9f48f78459168e
Shinsuke KUROKI <s-kuroki@aiming-inc.com>
2013-10-05 17:05:40 +0900

    Add JavaVersionParser.parse

--- lib/java_version_parser.rb
+++ lib/java_version_parser.rb
@@ -1,5 +1,17 @@
 class JavaVersionParser
-  def self.judge_validity(source)
-    /^JDK\d+u\d+$/ === source
+  PATTERN = /^JDK(\d+)u(\d+)$/
+  Result = Struct.new(:family_number, :update_number)
+  class InvalidSource < StandardError; end
+
+  class << self
+    def judge_validity(source)
+      PATTERN === source
+    end
+
+    def parse(source)
+      m = PATTERN.match(source)
+      raise InvalidSource if m.nil?
+      Result.new(m[1].to_i, m[2].to_i)
+    end
   end
 end
--- spec/lib/java_version_parser_spec.rb
+++ spec/lib/java_version_parser_spec.rb
@@ -7,4 +7,16 @@ describe JavaVersionParser do
     specify { JavaVersionParser.judge_validity("JDK7u").should be_false }
     specify { JavaVersionParser.judge_validity("JDK7u9x").should be_false }
   end
+
+  describe '.parse' do
+    specify 'returns instance which knows family number and update number' do
+      v = JavaVersionParser.parse("JDK7u40")
+      v.family_number.should == 7
+      v.update_number.should == 40
+    end
+
+    specify 'raises when invalid source given' do
+      expect { JavaVersionParser.parse("JDK7u9x") }.to raise_error JavaVersionParser::InvalidSource
+    end
+  end
 end
