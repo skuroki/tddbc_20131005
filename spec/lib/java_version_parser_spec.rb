858017b19709edb70efa6bb9ae9b07327bdd0057
Shinsuke KUROKI <s-kuroki@aiming-inc.com>
2013-10-05 16:47:24 +0900

    Add aditional case

--- spec/lib/java_version_parser_spec.rb
+++ spec/lib/java_version_parser_spec.rb
@@ -5,5 +5,6 @@ describe JavaVersionParser do
     specify { JavaVersionParser.judge_validity("JDK7u40").should be_true }
     specify { JavaVersionParser.judge_validity("JDK740").should be_false }
     specify { JavaVersionParser.judge_validity("JDK7u").should be_false }
+    specify { JavaVersionParser.judge_validity("JDK7u9x").should be_false }
   end
 end
